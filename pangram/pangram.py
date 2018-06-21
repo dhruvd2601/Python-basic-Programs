d741060bc39253ac77861d4ff874b3ce13b20b18
Dhruv Modi <dhruvd2601@gmail.com>
2018-06-21 09:59:54 +0530

    pangram commit

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,18 @@
 def is_pangram(sentence):
+    List = []
 
+    for i in range(26):
+        List.append(False)
+
+
+    for c in sentence.lower():
+        print(c)
+        if not c == " ":
+            List[ord(c) - ord('a')] = True
+        elif c.isdigit() :
+            List[c+ord('a')] = True
+
+    for ch in List:
+        if ch == False:
+            return False
+    return True
